e5608b83a21b9723e5239457eccafd71d09369ee
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 10:21:10 -0700

    Return api results to HouseDisplay container

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -7,8 +7,7 @@ const getHouses = async () => {
     const cleanObj = await cleanHouseData(fetchedObj);
 
     if (initialFetch.status <= 200) {
-      return fetchedObj
-      console.log(fetchedObj)
+      return cleanObj
     } else {
       throw new Error('Bad status')
     }
@@ -18,8 +17,8 @@ const getHouses = async () => {
 }
 
 const cleanHouseData = async (fetchedObj) => {
-  const cleanedHouses = fetchedObj.map( (house) => {
-    console.log({
+  const cleanedHouses = await fetchedObj.map( (house) => {
+    return {
       house: house.name,
       seats: house.seats,
       titles: house.titles,
@@ -28,59 +27,12 @@ const cleanHouseData = async (fetchedObj) => {
       swornMembers: house.swornMembers,
       words: house.words
 
-    }) 
-    
+    }
   })
-  // want to map through fetched obj
-  // for each house
-  // create an object with the housename as a key
-  // and key value pairs of the following:
-  // founded, seats, titles, ancestral weapons, coat of arms, sworm members, words
-  // spread those objects into an array
-  // do a promise.all of the objects
-  // return the array to the getHouses obj so it can go into the store
-  // await console.log(fetchedObj)
+ return cleanedHouses
 }
 
 
-// mock data from docs
-//   {  
-//      url:"https://www.anapioficeandfire.com/api/houses/81",
-//      name:"House Corbray of Heart's Home",
-//      region:"",
-//      coatOfArms:"Three black ravens in flight, holding three red hearts, on a white field(Argent, three ravens volant sable, each clutching in their claws a heart gules)",
-//      words:"",
-//      titles:[  
-//         "Lord of Heart's Home",
-//         "King of the Fingers (historical)"
-//      ],
-//      seats:[  
-//         "Heart's Home"
-//      ],
-//      currentLord:"https://www.anapioficeandfire.com/api/characters/681",
-//      heir:"https://www.anapioficeandfire.com/api/characters/677",
-//      overlord:"https://www.anapioficeandfire.com/api/houses/7",
-//      founded:"",
-//      founder:"https://www.anapioficeandfire.com/api/characters/256",
-//      diedOut:"",
-//      ancestralWeapons:[  
-//         "Lady Forlorn"
-//      ],
-//      cadetBranches:[  
-
-//      ],
-//      swornMembers:[  
-//         "https://www.anapioficeandfire.com/api/characters/255",
-//         "https://www.anapioficeandfire.com/api/characters/256",
-//         "https://www.anapioficeandfire.com/api/characters/447",
-//         "https://www.anapioficeandfire.com/api/characters/654",
-//         "https://www.anapioficeandfire.com/api/characters/677",
-//         "https://www.anapioficeandfire.com/api/characters/681",
-//         "https://www.anapioficeandfire.com/api/characters/871",
-//         "https://www.anapioficeandfire.com/api/characters/1182"
-//      ]
-//   }
-// ]
 // data formatting helper function here
 
 export { getHouses }
