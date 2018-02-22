e920f07e82f32f9bca6b15f2af543af97d3b7b89
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 10:36:29 -0700

    Add action test for LOAD_HOUSES

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,11 +1,16 @@
-// action tests go here
-
 import * as actions from './index';
 import mockData from '../helper/mockData'
 
 describe('all actions', () => {
 
   it('should return a type of LOAD_HOUSES', () => {
+    const houses = mockData;
 
+    const expected = {
+      type: 'LOAD_HOUSES',
+      houses
+    }
+    expect(actions.LOAD_HOUSES(houses)).toEqual(expected);
   })
+
 })
