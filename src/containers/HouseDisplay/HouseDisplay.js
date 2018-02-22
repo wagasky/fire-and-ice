cebb87ee8c781ba9ff430f4f791d39cb969d3b9a
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 09:53:47 -0700

    Attempt initial api call in helper and trigger call through componentDidMount and pull HouseDisplay container into App

--- src/containers/HouseDisplay/HouseDisplay.js
+++ src/containers/HouseDisplay/HouseDisplay.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fakeAction } from '../../actions';
 import PropTypes, { string } from 'prop-types';
+import { getHouses } from '../../helper/apiCalls.js'
 
 
 export class HouseDisplay extends Component {
 
+  async componentDidMount() {
+    await getHouses();
+  }
+
   render() {
     return (
       <div className='HouseDisplay'>
+        <p>I'm the HouseDisplay</p>
       </div>
     );
   }
--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -1,3 +1,22 @@
 // fetch call here
+const getHouses = async () => {
+  try {
+    const url = 'https://www.anapioficeandfire.com/api/houses'
+    const initialFetch = await fetch(url);
+    const fetchedObj = await initialFetch.json();
+
+    if (initialFetch.status <= 200) {
+      return fetchedObj
+      debugger
+    } else {
+      throw new Error('Bad status')
+    }
+  } catch (err) {
+    throw new Error('Error in getHouses fetch')
+  }
+}
+
 
 // data formatting helper function here
+
+export { getHouses }
