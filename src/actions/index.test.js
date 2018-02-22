d8b40423267cced39c03536bbea84abf9672cc50
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 10:30:37 -0700

    Adding mockData to write action tests for putting the houses in the store

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,1 +1,11 @@
 // action tests go here
+
+import * as actions from './index';
+import mockData from '../helper/mockData'
+
+describe('all actions', () => {
+
+  it('should return a type of LOAD_HOUSES', () => {
+
+  })
+})
---
+++ src/helper/mockData.js
@@ -0,0 +1,33 @@
+const mockData = [
+
+[
+     name:"House Corbray of Heart's Home",
+     coatOfArms:"Three black ravens in flight, holding three red hearts, on a white field(Argent, three ravens volant sable, each clutching in their claws a heart gules)",
+     words:"",
+     titles:[  
+        "Lord of Heart's Home",
+        "King of the Fingers (historical)"
+     ],
+     seats:[  
+        "Heart's Home"
+     ],
+     founded:"",
+     founder:"https://www.anapioficeandfire.com/api/characters/256",
+  
+     ancestralWeapons:[  
+        "Lady Forlorn"
+     ],
+     swornMembers:[  
+        "https://www.anapioficeandfire.com/api/characters/255",
+        "https://www.anapioficeandfire.com/api/characters/256",
+        "https://www.anapioficeandfire.com/api/characters/447",
+        "https://www.anapioficeandfire.com/api/characters/654",
+        "https://www.anapioficeandfire.com/api/characters/677",
+        "https://www.anapioficeandfire.com/api/characters/681",
+        "https://www.anapioficeandfire.com/api/characters/871",
+        "https://www.anapioficeandfire.com/api/characters/1182"
+     ]
+  }
+]
+
+]
