eb4803d1ba6cd35b4ea5e7f03b45c0a5744e8777
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 15:59:49 -0700

    Add final apiCall tests

--- src/helper/apiCalls.test.js
+++ src/helper/apiCalls.test.js
@@ -22,7 +22,55 @@ describe('apiCalls', () => {
     })
 
     it('should return the houses if the status is okay', () => {
-      const response = getHouses()
+      const expected = mockHouses
+      const response = getHouses('http://localhost:3001/api/v1/houses')
+
+      expect(response).resolves.toEqual(expected);
+    })
+
+    it('should throw an error if fetch fails', () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        status: 404
+      }))
+      const response = getHouses();
+      const expected = Error('Error in getHouses fetch')
     })
   })
+
+  describe('getSwornMembers', () => {
+    const mockHouses = ['house1', 'house2'];
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve(mockHouses)
+    }));
+
+    it('should be called with the correct params', () => {
+      const url = 'http://localhost:3001/api/v1/houses';
+
+      expect(window.fetch).not.toHaveBeenCalled();
+
+      getSwornMembers();
+
+      expect(window.fetch).toHaveBeenCalled(url);
+    })
+
+    it('should return the houses if the status is okay', () => {
+      const expected = ['member', 'member']
+      const url = 'http://localhost:3001/api/v1/character/:200'
+      const response = getSwornMembers('')
+
+      expect(response).resolves.toEqual(expected);
+    })
+
+    it('should throw an error if fetch fails', () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        status: 404
+      }))
+      const response = getSwornMembers();
+      const expected = Error('Error in getSwornMembers fetch')
+    })
+  })
+
+
 })
