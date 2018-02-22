8aae2014279f17c2990e3da89b04e95762bac4da
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 10:46:12 -0700

    Add housesReducer tests

--- src/reducers/housesReducer/housesReducer.test.js
+++ src/reducers/housesReducer/housesReducer.test.js
@@ -1,1 +1,19 @@
-// houses reducer tests go here
+import { housesReducer } from './housesReducer';
+import * as actions from '../../actions/index';
+import mockData from '../../helper/mockData'
+
+describe('housesReducer', () => {
+  let mockData;
+
+  it('should return the default state', () => {
+    const expected = [];
+
+    expect(housesReducer(undefined, {})).toEqual(expected);
+  })
+
+  it('should return the state with an array of houses', () => {
+    const expected = mockData;
+    
+    expects(housesReducer(undefined, actions.loadHouses(mockData)).toEqual(expected))
+  })
+})
