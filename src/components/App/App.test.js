52ac17fc6a8a34ce15342d337d0a6ea8a5ba6f7c
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-22 13:40:06 -0700

    Add component tests

---
+++ src/components/App/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow} from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+})
